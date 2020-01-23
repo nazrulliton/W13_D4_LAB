8b26fb80f2c11f42b954af906bd3c3b208a2b468
naz_liton <nazrulliton@icloud.com>
2020-01-23 10:49:27 +0000

    folder struc created

---
+++ src/main/java/com/codeclan/example/bookSystem/models/Booking.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.bookSystem.models;
+
+public class Booking {
+}
---
+++ src/test/java/com/codeclan/example/bookSystem/BookSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.bookSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
