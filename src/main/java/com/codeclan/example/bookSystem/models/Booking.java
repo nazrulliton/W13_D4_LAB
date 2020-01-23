7f589a2d75e17f3c6325c7f276316abf2cb57a9a
naz_liton <nazrulliton@icloud.com>
2020-01-23 11:00:55 +0000

    models created, mapping to follow

--- src/main/java/com/codeclan/example/bookSystem/models/Booking.java
+++ src/main/java/com/codeclan/example/bookSystem/models/Booking.java
@@ -1,4 +1,38 @@
 package com.codeclan.example.bookSystem.models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bookings")
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "date")
+    private String date;
+
+    public Booking(String date) {
+        this.date = date;
+    }
+
+    public Booking(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
--- src/main/java/com/codeclan/example/bookSystem/models/Course.java
+++ src/main/java/com/codeclan/example/bookSystem/models/Course.java
@@ -1,4 +1,62 @@
 package com.codeclan.example.bookSystem.models;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "courses")
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "town")
+    private String town;
+    @Column(name = "rating")
+    private int rating;
+
+    public Course(String name, String town, int rating) {
+        this.name = name;
+        this.town = town;
+        this.rating = rating;
+    }
+
+    public Course(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 }
